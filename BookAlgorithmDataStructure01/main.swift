2ca758a0ea6e2d49c22cc54ef0bbadcf14b3d202
shi-n <shi-n_xyz@cloudsquare.jp>
2020-01-11 15:52:51 +0900

    Initial Commit

---
+++ BookAlgorithmDataStructure01/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BookAlgorithmDataStructure01
+//
+//  Created by Shin Inaba on 2020/01/11.
+//  Copyright © 2020 Shin Inaba. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
